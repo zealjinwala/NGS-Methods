b5901793e6c3d81999f380e8e9e9ef5e2ddf4b1b
Zeal Jinwala <zsj24@drexel.edu>
2022-01-27 13:29:08 -0500

    script

---
+++ NeedlemanWunsch.py
@@ -0,0 +1,30 @@
+# By Zeal Jinwala
+# Date: January 27, 2022
+
+# This program is algorithm used in bioinformatics to align protein or nucleotide sequences. 
+# It is an application of dynamic programming to compare biological sequences.
+import pandas
+from Bio import SeqIO
+
+# Ask for seq 1, seq 2, match, mismatch, and gap
+def parseArgs():
+    print()
+    print("Hello World 4!")
+
+def highestScore( ):
+    print("Hello World 1!")
+
+def seqAlign():
+    print("Hello World 2!")
+
+def seqAlignBiopython():
+    print("Hello World 3!")
+
+def main():
+    parseArgs()
+    highestScore( )
+    seqAlign()
+    seqAlignBiopython()
+
+if __name__ == "__main__":
+    main()
