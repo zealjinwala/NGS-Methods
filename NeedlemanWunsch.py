80a2755673e714c9c01fd1499ed5210d88836e39
Zeal Jinwala <zsj24@drexel.edu>
2022-01-27 21:39:19 -0500

    global alignment

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -3,6 +3,8 @@
 
 # This program is algorithm used in bioinformatics to align protein or nucleotide sequences. 
 # It is an application of dynamic programming to compare biological sequences.
+
+from re import sub
 import pandas as pd
 import numpy as np
 from Bio import SeqIO
@@ -15,42 +17,26 @@ def parseArgs():
     mismatch = input("Enter the mismatch score: ")
     gap = input("Enter the gap penalty: ")
     score = seqAlign(seq1, seq2, match, mismatch, gap)
-    return score
+
 
 def highestScore(subMat,gap):
     rows = len(subMat)
     cols = len(subMat[0])
-    weight = subMat[1,1]
-    print(rows)
-    dynamicTable = np.zeros((rows,cols), dtype=int)
+    dynamicTable = np.zeros((rows,cols),dtype=int)
+    dynamicTable[0,1:cols] = dynamicTable[0,1:cols]  + gap
+    dynamicTable[1:rows,0] = dynamicTable[1:rows,0] + gap
     for r in range(1,rows):
         for c in range(1,cols):
             curr = subMat[r,c]
-            if r+c != 2:
-                if c>1: 
-                    fromLeft = dynamicTable[r,c-1] + gap
-                if r>1: 
-                    fromTop = dynamicTable[r-1,c] + gap
-                if (r>1) and (c>1): 
-                    topLeft = dynamicTable[r-1,c-1]
-                    fromDiag = curr + topLeft
-                # FIX ME: -----------------                
-                if fromLeft and fromTop and fromDiag:
-                    weight = max([fromLeft, fromTop, fromDiag])
-                    del fromTop
-                    del fromLeft
-                    del fromDiag
-                elif fromTop and fromDiag:
-                    weight = fromLeft
-                    del fromLeft
-                elif fromLeft and fromDiag:
-                    weight = fromTop
-                    del fromTop
-                # ----------------- 
-            dynamicTable[r,c] = weight
-    score = dynamicTable[rows, cols]
+            diag = curr + dynamicTable[r-1,c-1]
+            top = dynamicTable[r-1,c]+gap
+            left = dynamicTable[r,c-1]+gap
+            maxScore = max(diag,top,left)
+            dynamicTable[r,c] = maxScore
+            print(dynamicTable)
+    score = dynamicTable[rows-1, cols-1]
+    print(score)
     
-
 def seqAlign(seq1, seq2, match, mismatch, gap):
     if not match:
         match = 5
@@ -58,22 +44,19 @@ def seqAlign(seq1, seq2, match, mismatch, gap):
         mismatch = -4
     if not gap:
         gap = -5
-
     match = int(match)
     mismatch = int(mismatch)
     gap = int(gap)
-
     lenSeq1 = len(seq1)
     lenSeq2 = len(seq2)
     subMat = np.zeros((lenSeq1+1,lenSeq2+1), dtype=int)
     for i in range(lenSeq1):
         for j in range(lenSeq2):
             if seq1[i] == seq2[j]: 
-                subMat[i+1,j+1] = 1
+                subMat[i+1,j+1] = match
             else:
-                subMat[i+1,j+1] = 0
+                subMat[i+1,j+1] = mismatch
     highestScore(subMat, gap)
-    
 
 # def seqAlignBiopython():
 #     print("Hello World 3!")
