dd1bc44d96bd4044205ec928260802b03bac8ff4
Zeal Jinwala <zsj24@drexel.edu>
2022-01-28 11:10:08 -0500

    needs to print aignment

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -1,6 +1,5 @@
 # By Zeal Jinwala
 # Date: January 27, 2022
-
 # This program is algorithm used in bioinformatics to GLOBALY align protein or nucleotide sequences. 
 # It is an application of dynamic programming to compare biological sequences.
 
@@ -8,29 +7,26 @@ import pandas as pd
 import numpy as np
 from Bio import SeqIO
 
-def parseArgs():
-    seq1 = input("Enter the first sequence: ")
-    seq2 = input("Enter the second sequence: ")
-    match = input("Enter the match score: ")
-    mismatch = input("Enter the mismatch score: ")
-    gap = input("Enter the gap penalty: ")
-    score = seqAlign(seq1, seq2, match, mismatch, gap)
-    print(score)
-
 def highestScore(subMat,gap):
-    rows = len(subMat)-1
-    cols = len(subMat[0])-1
+    rows = len(subMat)
+    cols = len(subMat[0])
     dynamicTable = np.zeros((rows,cols),dtype=int)
+    top = gap + gap*max(rows,cols)
+    diag = gap + gap*max(rows,cols)
     for r in range(rows):
         for c in range(cols):
-            curr = subMat[r,c]
-            diag = curr + dynamicTable[r-1,c-1]
-            top = dynamicTable[r-1,c] + gap
-            left = dynamicTable[r,c-1] + gap
-            maxScore = max(diag,top,left)
-            dynamicTable[r,c] = maxScore
-            print(dynamicTable)
+            if r + c > 0:
+                curr = subMat[r,c]
+                if c>0 and r>0:
+                    diag = curr + dynamicTable[r-1,c-1]
+                if r>0:
+                    top = dynamicTable[r-1,c] + gap
+                if c>0:
+                    left = dynamicTable[r,c-1] + gap
+                maxScore = max(diag,top,left)
+                dynamicTable[r,c] = maxScore
     score = dynamicTable[rows-1, cols-1]
+    return score 
     
 def seqAlign(seq1, seq2, match, mismatch, gap):
     if not match:
@@ -51,13 +47,21 @@ def seqAlign(seq1, seq2, match, mismatch, gap):
                 subMat[i+1,j+1] = match
             else:
                 subMat[i+1,j+1] = mismatch
-    highestScore(subMat, gap)
+    score = highestScore(subMat, gap)
+    return score
 
 # def seqAlignBiopython():
 #     print("Hello World 3!")
 
 def main():
-    parseArgs()
+    seq1 = input("Enter the first sequence: ")
+    seq2 = input("Enter the second sequence: ")
+    match = input("Enter the match score: ")
+    mismatch = input("Enter the mismatch score: ")
+    gap = input("Enter the gap penalty: ")
+    score = seqAlign(seq1, seq2, match, mismatch, gap)
+    print(score)
+
 
 if __name__ == "__main__":
     main()
