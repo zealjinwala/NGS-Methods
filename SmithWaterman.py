556151eaeab69f9a82c1d336c4a8fefc22dee6c7
Zeal Jinwala <zsj24@drexel.edu>
2022-06-21 12:50:42 -0400

    remove incomplete files

--- SmithWaterman.py
+++
@@ -1,9 +0,0 @@
-# By Zeal Jinwala
-# Date: January 27, 2022
-
-# This program is algorithm used in bioinformatics to LOCALY align protein or nucleotide sequences. 
-# It is an application of dynamic programming to compare biological sequences.
-# Instead of looking at the entire sequence, the Smith–Waterman algorithm compares segments of all possible lengths and optimizes the similarity measure.
-
-
-
